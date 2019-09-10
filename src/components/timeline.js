53aa77cf9566257c553280703e96d4c9238a1719
vcostap <viniciuscostapereira@hotmail.com>
2019-09-10 16:38:40 -0300

    Second commit

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,47 +1,32 @@
 import React, {Component} from 'react';
 import TimelineBox from './timelineBox';
 import axios from 'axios';
-import './timeline.css';
 
 export default class Timeline extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			isLoading: true,
-			finalPurchases: [],
-			productsPurchases: []
+			compraFinal: [],
+			produtosComprados: []
 		}
 	}
 
 	componentDidMount() {
 		axios.get('https://storage.googleapis.com/dito-questions/events.json')
 		.then(response => {
-			let finalPurchasesArray = response.data.events.filter( purchase => purchase.event === 'comprou');
-			let productsPurchasesArray = response.data.events.filter( product => product.event === 'comprou-produto');
+			let compraFinal_Array = response.data.events.filter( purchase => purchase.event === 'comprou');
+			let produtosComprados_Array = response.data.events.filter( product => product.event === 'comprou-produto');
 			this.setState({
-				finalPurchases: finalPurchasesArray,
-				productsPurchases: productsPurchasesArray,
-				isLoading: false
+				compraFinal: compraFinal_Array,
+				produtosComprados: produtosComprados_Array
 			});
 		})
 		.catch(console.log);
 	}
 
-	separatePurchases() {
-	}
-
 	render() {
-		if(!this.state.isLoading){
-			return (
-				<div class="purchase-timeline">
-					<TimelineBox finalPurchases={this.state.finalPurchases} productsPurchases={this.state.productsPurchases} />
-				</div>
-			);
-		}
-		else {
-			return (
-				<div>Carregando...</div>
-			)
-		}
+		return (
+			<TimelineBox finalPurchases={this.state.compraFinal} productsPurchases={this.state.produtosComprados} />
+		);
 	}
 }
